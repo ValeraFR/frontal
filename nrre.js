b0c9957e94d7a903f09cd77a70fe755ac5bc1c00
Valerii <valerakandebai@example.com>
2023-01-03 15:31:59 +0100

    first commit

---
+++ nrre.js
@@ -0,0 +1,23 @@
+'use strict';
+
+console.log('arr' + " -object");
+console.log(4 + +"");
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // dwcer--;
+
+    console.log(++incr);
+    console.log(--decr);
+
+    console.log(5%2);
+
+    console.log(2 + 2 * 2 !== '6');
+
+    const isChecked = false,
+            isClose = false;
+
+    console.log(isChecked || !isClose);
+
